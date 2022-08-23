3c73791b2014f7d3842ebbcd81c1a07f67d67f35
alisson-ap <alisson08silva@gmail.com>
2022-08-23 09:45:42 -0300

    Listagem Funcionario

--- Frontend/jsP/Script.js
+++ Frontend/jsP/Script.js
@@ -1,5 +1,49 @@
 const btn = document.getElementById("sendC");
 
+const tableId = document.getElementById("dataBody");
+
+async function listar() {
+
+    const results = await fetch("http://localhost:8080/funcao");
+    const data = await results.json();
+
+    data.map((post) => {
+        const tr = document.createElement("tr");
+        const th = document.createElement("th");
+        const td = document.createElement("td");
+
+        th.setAttribute("scope", "col");
+
+        th.innerText = post.id;
+        td.innerText = post.nomeFuncao;
+
+        tr.appendChild(th);
+        tr.appendChild(td);
+        tableId.appendChild(tr);
+
+    });
+}
+
+listar();
+
+
+function adiconar(datas) {
+
+    const tr = document.createElement("tr");
+    const th = document.createElement("th");
+    const td = document.createElement("td");
+
+    th.setAttribute("scope", "col");
+
+    th.innerText = datas.id;
+    td.innerText = datas.nomeFuncao;
+
+    tr.appendChild(th);
+    tr.appendChild(td);
+    tableId.appendChild(tr);
+
+}
+
 btn.addEventListener("click", async function (e) {
 
     e.preventDefault();
@@ -24,6 +68,7 @@ btn.addEventListener("click", async function (e) {
         }
 
         const response = await fetch("http://localhost:8080/funcao", init);
+        console.log(response);
 
     }
 
